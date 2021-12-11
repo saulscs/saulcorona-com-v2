cdeb88dfb2bacf53844004987ac6daa47e45eb9d
Saul Corona <saulcoronascs@gmail.com>
2021-12-11 14:38:39 -0600

    skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ if(navClose){
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
@@ -34,7 +34,24 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 
